76719b68044f930dac1e687cafe321ec176c9c73
MIDDLEEAST\barakkinarti <barakkinarti@microsoft.com>
2022-02-22 14:46:52 +0200

    input via args

--- hashtagify.js
+++ hashtagify.js
@@ -5,13 +5,9 @@ hashtagify.doFollowedByNumber = false;
 
 function getHashTags(text) {
   var title = text;
-
   var content = text;
-
   var vocabulary = [];
-
   var textWithHashtags = hashtagify(title, content, vocabulary);
-
   const hashes = extractHashTagsFromText(textWithHashtags);
 
   return hashes;
@@ -19,22 +15,22 @@ function getHashTags(text) {
 
 function extractHashTagsFromText(inputText) {
   var regex = /(?:^|\s)(?:#)([a-zA-Z\d]+)/gm;
-
   var matches = [];
-
   var match;
-
   while ((match = regex.exec(inputText))) {
     matches.push(match[1]);
   }
 
   return [...new Set(matches)];
 }
-const data = fs.readFileSync("./input.txt", { encoding: "utf8", flag: "r" });
+const myArgs = process.argv.slice(2);
+const inputFile = myArgs[0] || "./input.txt";
+const outputFile = myArgs[1] || "hashtagify_output.txt";
+const data = fs.readFileSync(inputFile, { encoding: "utf8", flag: "r" });
 const res = getHashTags(data);
 
 const resStr = JSON.stringify(res, true, 4);
-fs.writeFileSync("hashtagify_output.txt", resStr, {
+fs.writeFileSync(outputFile, resStr, {
   encoding: "utf8",
 });
 console.log(resStr);
--- rake.js
+++ rake.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
 var rake = require("rake-modified").default;
-const data = fs.readFileSync("./input.txt", { encoding: "utf8", flag: "r" });
+const myArgs = process.argv.slice(2);
+const inputFile = myArgs[0] || "./input.txt";
+const outputFile = myArgs[1] || "rake_output.txt";
+const data = fs.readFileSync(inputFile, { encoding: "utf8", flag: "r" });
 const res = rake(
     data
 );
 
 const resStr = JSON.stringify(res, true, 4);
-fs.writeFileSync("rake_output.txt",
+fs.writeFileSync(outputFile,
 resStr,
     {
       encoding: "utf8",
